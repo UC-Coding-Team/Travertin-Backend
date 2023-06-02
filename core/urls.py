b8b490001aef4cb77b6ba939aade78753d2e79ac
Abdulla Abdukulov <abduqulovabdulla3108@gmail.com>
2023-06-02 19:09:28 +0500

    added product part

--- core/urls.py
+++ core/urls.py
@@ -1,18 +1,20 @@
 from django.urls import path
 
-from core.views.category import CategoryListCreateView, CategoryRetrieveUpdateDestroyView
 from core.views.discount import DiscountListCreateView, DiscountRetrieveUpdateDestroyView
 from core.views.outsite import NavbartexListCreateView, NavbartexRetrieveUpdateDestroyView
-from core.views.product import ProductRetrieveUpdateDestroyView, ProductListCreateView
+from core.views.category import CategoryListCreateView
+from core.views.product import (
+    ProductRetrieveView,
+    CategoryProductListView
+)
+
 
 app_name = 'core'
 
 urlpatterns = [
     path('categories/', CategoryListCreateView.as_view(), name='category-list'),
-    path('categories/<int:pk>/', CategoryRetrieveUpdateDestroyView.as_view(), name='category-detail'),
-    path('products/', ProductListCreateView.as_view(), name='product-list'),
-    path('products/<int:pk>/', ProductRetrieveUpdateDestroyView.as_view(), name='product-detail'),
-
+    path('products/<int:pk>/', ProductRetrieveView.as_view(), name='product-detail'),
+    path('categories/<int:pk>/', CategoryProductListView.as_view(), name='category-product-list'),
     path('discounts/', DiscountListCreateView.as_view(), name='discount-list-create'),
     path('discounts/<int:pk>/', DiscountRetrieveUpdateDestroyView.as_view(), name='discount-retrieve-update-destroy'),
     path('navbartex/', NavbartexListCreateView.as_view(), name='navbartex-list-create'),
--- core/views/product.py
+++ core/views/product.py
@@ -1,18 +1,24 @@
 from rest_framework import generics
+from rest_framework.response import Response
 
-from core.models import Product
+from core.models import Product, Category
 from core.serializers.product import ProductSerializer, ProductDetailSerializer
 
 
-class ProductListCreateView(generics.ListCreateAPIView):
+class ProductRetrieveView(generics.RetrieveAPIView):
     queryset = Product.objects.all()
+    serializer_class = ProductDetailSerializer
+
+    def get(self, request, *args, **kwargs):
+        return self.retrieve(request, *args, **kwargs)
+
+
+class CategoryProductListView(generics.RetrieveAPIView):
+    queryset = Category.objects.all()
     serializer_class = ProductSerializer
 
-
-class ProductRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
-    queryset = Product.objects.all()
-
-    def get_serializer_class(self):
-        if self.request.method == 'GET':
-            return ProductDetailSerializer
-        return ProductSerializer
+    def get(self, request, *args, **kwargs):
+        instance = self.get_object()
+        products = Product.objects.filter(category=instance)
+        serializer = self.get_serializer(products, many=True)
+        return Response(serializer.data)
