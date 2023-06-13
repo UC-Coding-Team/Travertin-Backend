2419923cd6d43a0da784d925fbe26ab30c384dd9
abd1bayev <jasurabdibayev0@gmail.com>
2023-06-13 14:19:09 +0500

    add

--- config/settings.py
+++ config/settings.py
@@ -47,7 +47,11 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = 'config.urls'
-CORS_ORIGIN_ALLOW_ALL = True
+CORS_ORIGIN_ALLOW_ALL = False
+
+CORS_ORIGIN_WHITELIST = [
+    'http://localhost:5173',  # Add the origin of your React application
+]
 
 TEMPLATES = [
     {
