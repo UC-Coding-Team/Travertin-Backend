f5facd2b70f21944f5f5e80566511a85ddd4ab22
abd1bayev <jasurabdibayev0@gmail.com>
2023-06-13 14:57:18 +0500

    update cors

--- config/settings.py
+++ config/settings.py
@@ -19,14 +19,14 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    # 'corsheaders',
+    'corsheaders',
     'drf_yasg',
     'rest_framework',
     'core',
 ]
 
 MIDDLEWARE = [
-    # 'corsheaders.middleware.CorsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -39,11 +39,8 @@ MIDDLEWARE = [
 
 ROOT_URLCONF = 'config.urls'
 
-# CORS_ORIGIN_ALLOW_ALL = False
-
-# CORS_ORIGIN_WHITELIST = [
-#     'http://localhost:5173',  # React uygulamasının kökenini buraya ekleyin
-# ]
+CORS_ALLOW_CREDENTIALS = True
+CORS_ALLOW_ALL_ORIGINS = True
 
 TEMPLATES = [
     {
